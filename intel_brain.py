93194e015c3342ec12d3901092a140d8c2bc8928
Subrat Pandey <subratpandey974@gmail.com>
2025-03-27 14:10:43 -0500

    set up multi-model-llm response

--- intel_brain.py
+++ intel_brain.py
@@ -16,8 +16,33 @@ image_file = open(image_path, "rb")
 
 encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
 
+client = Groq()
+query = "is there something wrong with my head?"
+model = "llama-3.2-90b-vision-preview"
 
+messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text", 
+                    "text": query
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{encoded_image}",
+                    },
+                },
+            ],
+        }]
+    
+chat_completion = client.chat.completions.create(
+    messages = messages,
+    model = model
+)
 
+print(chat_completion.choices[0].message.content)
 
 
 
